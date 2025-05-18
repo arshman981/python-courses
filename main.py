7a77563c6dcc4bc75ad1d260fb0af9000ab6d561
arshman981 <arshmannajam10@gmail.com>
2025-05-18 14:30:17 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print ("hello world")
+import re, randomfrom colorama import Fore, init
+
+#Initialize colorama (autoreset ensures each print resets after use)
+init(autoreset=True)
+
+#Destination & joke data
+destinations = {
+"beaches" ["Bali," "Maldives," "Phuket"],
+ "mountains": ["Swiss Alps", "Rocky Mountains", "Himalayas"],
+    "cities": ["Tokyo", "Paris", "New York"]
+}
+jokes = [
+    "Why don't programmers like nature? Too many bugs!",
+    "Why did the computer go to the doctor? Because it had a virus!",
+    "Why do travelers always feel warm? Because of all their hot spots!"
+]
+
+#Helper function to normalize
